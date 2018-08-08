bc789e5855f61cb7d1736686183ecc66a4449aea
“Tyler <tylerpalef@gmail.com>
2018-08-08 08:38:04 -0400

    added submit a photo button

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,4 +8,25 @@ class PicturesController < ApplicationController
      @picture = Picture.find(params[:id])
   end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+     @picture = Picture.new
+
+     @picture.title = params[:picture][:title]
+     @picture.artist = params[:picture][:artist]
+     @picture.url = params[:picture][:url]
+
+
+     if @picture.save
+     # if the picture gets saved, generate a get request to "/pictures" (the index)
+       redirect_to "/pictures"
+     else
+     # otherwise render new.html.erb
+       render :new
+     end
+  end
+  
 end
