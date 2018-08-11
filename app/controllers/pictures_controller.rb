183eb781287a34ff62a1b658fb45042a70fbf322
“Tyler <tylerpalef@gmail.com>
2018-08-10 21:59:42 -0400

    finished exercise from friday

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,8 @@
 class PicturesController < ApplicationController
 
   def index
-    @pictures = Picture.all
-  end
+   @most_recent_pictures = Picture.most_recent_five
+ end
 
   def show
      @picture = Picture.find(params[:id])
@@ -21,25 +21,25 @@ class PicturesController < ApplicationController
 
    end
 
-     def edit
-       @picture = Picture.find(params[:id])
-     end
-     #
-     def update
-       @picture = Picture.find(params[:id])
+   def edit
+     @picture = Picture.find(params[:id])
+   end
 
-       @picture.title = params[:picture][:title]
-       @picture.artist = params[:picture][:artist]
-       @picture.url = params[:picture][:url]
+   def update
+     @picture = Picture.find(params[:id])
+
+     @picture.title = params[:picture][:title]
+     @picture.artist = params[:picture][:artist]
+     @picture.url = params[:picture][:url]
 
 
      if @picture.save
-     # if the picture gets saved, generate a get request to "/pictures" (the index)
-       redirect_to "/pictures"
+       redirect_to "/pictures/#{@picture.id}"
      else
-     # otherwise render new.html.erb
-       render :new
+       render :edit
      end
+   end
+
 
      def destroy
        @picture = Picture.find(params[:id])
