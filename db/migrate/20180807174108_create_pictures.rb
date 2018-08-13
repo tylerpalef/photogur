0d7d81449a05f03a57f1c481370c80e603ee1aa6
“Tyler <tylerpalef@gmail.com>
2018-08-13 18:59:31 -0400

    finished validations

--- db/migrate/20180807174108_create_pictures.rb
+++ db/migrate/20180807174108_create_pictures.rb
@@ -1,4 +1,8 @@
 class CreatePictures < ActiveRecord::Migration[5.0]
+  validates :title, length: {within: 3..20}
+  validates :artist, :url, presence: true
+  validates :url, uniqueness: true
+
   def change
     create_table :pictures do |t|
       t.string :artist
@@ -9,3 +13,13 @@ class CreatePictures < ActiveRecord::Migration[5.0]
     end
   end
 end
+
+#
+# artist:
+# be present (i.e. not blanks allowed)
+# title:
+# minimum length 3 characters
+# maximum length 20 characters
+# url:
+# be present (i.e. not blanks allowed)
+# be unique
